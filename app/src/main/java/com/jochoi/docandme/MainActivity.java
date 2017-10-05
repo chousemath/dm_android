8e1cb6dcf788aba220caaec63837da4d1e8f7341
Joseph Sungpil Choi <chousemath@gmail.com>
2017-10-05 16:08:01 +0900

    added some more click handlers

--- app/src/main/java/com/jochoi/docandme/MainActivity.java
+++ app/src/main/java/com/jochoi/docandme/MainActivity.java
@@ -1,5 +1,7 @@
 package com.jochoi.docandme;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("\n\n\nI am loaded...\n\n\n");
+    }
+
+    public void openSpecialtySelectionDialog(View view) {
+        AlertDialog.Builder specialtySelectionAlert = new AlertDialog.Builder(this);
+        specialtySelectionAlert.setTitle("Select a medical specialty");
+        specialtySelectionAlert.setPositiveButton("Select", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // action when clicking this button goes here...
+                System.out.println("specialty selection has been made");
+            }
+        });
+        specialtySelectionAlert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // action when clicking this button goes here...
+                System.out.println("specialty selection has been canceled");
+            }
+        });
+        specialtySelectionAlert.show();
+    }
+
+    public void openStationSelectionDialog(View view) {
+        AlertDialog.Builder stationSelectionAlert = new AlertDialog.Builder(this);
+        stationSelectionAlert.setTitle("Select a subway station near you");
+        stationSelectionAlert.setPositiveButton("Select", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // action when clicking this button goes here...
+                System.out.println("station selection has been made");
+            }
+        });
+        stationSelectionAlert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // action when click this button goes here...
+                System.out.println("station selection has been canceled");
+            }
+        });
+        stationSelectionAlert.show();
     }
 
     public void performSearch(View view) {
